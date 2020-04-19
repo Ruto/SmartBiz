256ca4790adc4bb67d74846b930444114d881f41
Ruto Ken <ruto.k.ken@gmail.com>
2020-04-19 13:49:32 +0300

    rake routes for forms and structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :v1, defaults: { format: :json } do
-    
+
     resources :users, only: [:create, :show]  do
        collection do
          get  'unconfirmed_users'
@@ -13,6 +13,20 @@ Rails.application.routes.draw do
        end
      end
 
+     resources :forms do
+       namespace :forms do
+         resources :consignment, :delivery_note, :dispatch_note, :goods_received, :invoice, :issue_note,
+          :job_card, :leave_application,:meal_card, :overtime, :petty_cash, :proforma_ivoice, :purchase_order,
+          :purchase, :quotation, :report, :requisition, :controller => 'forms', :except => [:destroy, :edit, :update]
+       end
+     end
+
+     resources :structures do
+          namespace :structures do
+            resources :incomes, :expenses, :indirect_expenses, :adminstrative_costs,
+            :controller => 'structures', :except => [:destroy, :edit, :update ]
+          end
+      end
   end
 
 
