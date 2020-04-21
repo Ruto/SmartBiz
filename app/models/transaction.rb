1f66ee921f9ed955ce7b1bc618f66818a8987d9b
Ruto Ken <ruto.k.ken@gmail.com>
2020-04-21 20:04:40 +0300

    images and transactions scaffolds deleted

--- app/models/transaction.rb
+++
@@ -1,7 +0,0 @@
-class Transaction < ApplicationRecord
-  belongs_to :transactionable, polymorphic: true
-  belongs_to :structure
-  belongs_to :image
-  belongs_to :account
-  belongs_to :user
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_193110) do
+ActiveRecord::Schema.define(version: 2020_04_20_194304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,18 @@ ActiveRecord::Schema.define(version: 2020_04_20_193110) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["structure_id"], name: "index_forms_on_structure_id"
     t.index ["user_id"], name: "index_forms_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.text "memo"
+    t.string "imageable_type", null: false
+    t.bigint "imageable_id", null: false
+    t.boolean "checked", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
   end
 
   create_table "structures", force: :cascade do |t|
