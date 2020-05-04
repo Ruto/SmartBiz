63a685477e01826c82e9537702254aa8aceba859
Ruto Ken <ruto.k.ken@gmail.com>
2020-05-04 21:09:18 +0300

    Structurable setup migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_230403) do
+ActiveRecord::Schema.define(version: 2020_05_04_170257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,19 @@ ActiveRecord::Schema.define(version: 2020_05_01_230403) do
     t.index ["revoked_by"], name: "index_profiles_on_revoked_by"
     t.index ["structure_id"], name: "index_profiles_on_structure_id"
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "structurables", id: false, force: :cascade do |t|
+    t.integer "structure_id"
+    t.integer "structured_id"
+    t.string "cost_code"
+    t.boolean "active", default: true
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["structure_id"], name: "index_structurables_on_structure_id"
+    t.index ["structured_id"], name: "index_structurables_on_structured_id"
+    t.index ["user_id"], name: "index_structurables_on_user_id"
   end
 
   create_table "structures", force: :cascade do |t|
@@ -165,6 +178,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_230403) do
   add_foreign_key "profiles", "images", column: "images_id"
   add_foreign_key "profiles", "structures"
   add_foreign_key "profiles", "users"
+  add_foreign_key "structurables", "users"
   add_foreign_key "structures", "users"
   add_foreign_key "transactions", "accounts"
   add_foreign_key "transactions", "images"
